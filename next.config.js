61a287d7fb0f5cd1f8e1359c6bedb8677cb346ad
Rama Agung Supriyadi <67418383+Bl4ck-lion@users.noreply.github.com>
2025-07-28 10:56:42 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-// next.config.js
 module.exports = {
   reactStrictMode: true,
-  // agar Vercel mem-publish folder `api/` sebagai Python Functions
-  functions: {
-    'api/**/*.py': { runtime: 'python3.9' }
-  }
-};
+- functions: {
+-   'api/**/*.py': { runtime: 'python3.9' }
+- }
+}
